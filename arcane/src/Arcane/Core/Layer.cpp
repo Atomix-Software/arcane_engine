8f370059de740b335f738fb7812ff96b25d6896b
ArcaneSunku <t4hnner@gmail.com>
2026-02-23 00:54:13 -0600

    Implemented Layers and a LayerStack to manage them

---
+++ arcane/src/Arcane/Core/Layer.cpp
@@ -0,0 +1,18 @@
+#include "aepch.h"
+#include "Arcane/Core/Layer.h"
+
+namespace Arcane
+{
+
+	Layer::Layer(const std::string& name) :
+		m_DebugName(name) 
+	{
+
+	}
+
+	Layer::~Layer()
+	{
+
+	}
+
+}
